5f5b823da77d9c98abe0f89f0d6f04e59077daad
이상봉 <bongbong9708@douzone.com>
2022-01-10 14:12:48 +0900

    스토리보드 없이 시작

---
+++ IDCardDemo/IDCardDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDCardDemo
+//
+//  Created by 이상봉 on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
